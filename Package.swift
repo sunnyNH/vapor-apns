966621ebc58f8e08000962c3badb7d81d7d38f55
牛辉 <niuhui@niuhui.local>
2017-09-22 00:23:05 +0800

    测试

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "VaporAPNS",
     targets: [],
     dependencies: [
+        
         .Package(url: "https://github.com/vapor/json.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/console.git", majorVersion: 2),
